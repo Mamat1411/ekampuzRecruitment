5ba7c419e06ed0c3af9d521b88230c3830daf6d5
Mamat1411 <muhammadmujahid14@gmail.com>
2023-09-05 15:47:37 +0700

    Fixing Answer Number 2

--- logicalTest/2_mysql_query.sql
+++ logicalTest/2_mysql_query.sql
@@ -1,5 +1,5 @@
-select mhs.mhs_nama, tmn.nilai from tb_mahasiswa_nilai tmn 
+select mhs.mhs_nama, max(tmn.nilai) from tb_mahasiswa_nilai tmn 
 left join tb_matakuliah mk 
 on mk.mk_id = tmn.mk_id
 left join tb_mahasiswa mhs
-on mhs.mhs_id = tmn.mhs_id where mk.mk_kode = "MK303" order by tmn.nilai desc 
+on mhs.mhs_id = tmn.mhs_id where mk.mk_kode = "MK303"
