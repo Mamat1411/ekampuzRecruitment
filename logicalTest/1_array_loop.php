541a85ff901b239c92078599a5ec2664ec3a367c
Mamat1411 <muhammadmujahid14@gmail.com>
2023-09-05 14:10:32 +0700

    Question Number 1 Done with evidence

--- logicalTest/1_array_loop.php
+++ logicalTest/1_array_loop.php
@@ -5,9 +5,11 @@
     $aplikasi[4] = 'eCampuz';
     $aplikasi[5] = 'eOviz';
 
-    $iteration = count($aplikasi);
-
-    while ($a <= $iteration) {
-        echo $aplikasi[$iteration];
+    $iteration = 1;
+    $application = count($aplikasi);
+    
+    while ($iteration <= $application) {
+        echo $aplikasi[$iteration] . "<br><br>";
+        $iteration++;
     }
 ?>
